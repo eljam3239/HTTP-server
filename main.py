10c383ccfb0c7c26fc9f02df93e2d0be558e84c6
Eli <elijames2301@gmail.com>
2023-10-26 19:24:16 -0400

    Write 200 OK

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import socket
 
 
 def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    conn, addr = server_socket.accept()
+    data = conn.recv(1024)
+    response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    conn.send(response)
 
 if __name__ == "__main__":
     main()
