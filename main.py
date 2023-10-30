9a60c2789caf53aedc353510ea11e0bd03b42c07
Eli <elijames2301@gmail.com>
2023-10-29 23:45:52 -0400

    Change response to handle 200 and 404

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import socket
 
+def handle_data(data,conn):
+    data_str = data.decode()
+    data_line = data_str.split("\r\n")
+    path=data_line[0].split(" ")[1]
+    if path=="/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +16,9 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept()
     data = conn.recv(1024)
-    response = "HTTP/1.1 200 OK\r\n\r\n".encode()
-    conn.send(response)
+    handle_data(data,conn)
+    #response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    #conn.send(response)
 
 if __name__ == "__main__":
     main()
